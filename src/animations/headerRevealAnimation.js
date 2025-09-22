da90e65496202e71306a107d0dd9f5695eb3f7c7
VigilantV <mehrabgheibi@gmail.com>
2025-09-23 00:05:19 +0330

    feat: add svg animation to my name in landing

--- src/animations/headerRevealAnimation.js
+++ src/animations/headerRevealAnimation.js
@@ -4,14 +4,14 @@ const headerRevealAnimation = (animationRef) => {
   const ctx = gsap.context(() => {
     const tl = gsap.timeline({
       defaults: {
-        delay: 0.4,
-        duration: 0.7,
-        ease: Elastic.easeOut.config(1.6, 2),
-        stagger: 0.3,
+        delay: 0.5,
+        duration: 1.2,
+        ease: Elastic.easeOut.config(2.5, 2),
+        stagger: 0.5,
       },
     });
     tl.fromTo(
-      ["#greeting", "#name", "#profession"],
+      ["#greeting", "#profession"],
       { x: "-30vw", opacity: 0, scale: 0.5 },
       { x: 0, opacity: 1, scale: 1 }
     );
